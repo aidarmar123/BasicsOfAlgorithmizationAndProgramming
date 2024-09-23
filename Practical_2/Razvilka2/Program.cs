07eb2e09019568b7a50c29bf89b50ae876745675
Aidar <ajdar.mar@yandex.ru>
2024-09-23 23:12:00 +0300

    Made task №2

---
+++ Practical_2/Razvilka2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razvilka2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number");
+            var number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number is " + (number % 2 == 0 ? "even" : "odd"));
+        }
+    }
+}
