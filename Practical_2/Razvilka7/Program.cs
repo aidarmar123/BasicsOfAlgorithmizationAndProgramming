03c155647278220f85036a0388f58abfb42dfecf
Aidar <ajdar.mar@yandex.ru>
2024-09-24 22:38:11 +0300

    Made task №7

---
+++ Practical_2/Razvilka7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razvilka7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive");
+            }else if (number < 0)
+            {
+                Console.WriteLine("The number is negative");
+            }
+            else
+            {
+                Console.WriteLine("The nummber is zero");
+            }
+        }
+    }
+}
