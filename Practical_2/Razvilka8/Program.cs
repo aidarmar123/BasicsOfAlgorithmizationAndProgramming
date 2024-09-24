a6dd8b8753ccf2ac1987515e133de2328b5bb608
Aidar <ajdar.mar@yandex.ru>
2024-09-24 23:04:41 +0300

    Made task №8

---
+++ Practical_2/Razvilka8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razvilka8
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Input number day ");
+            var number = Convert.ToUInt32(Console.ReadLine());
+            var date = DateTime.MinValue.AddDays(number-1);
+            Console.WriteLine(date.ToString("dddd"));
+        }
+    }
+}
