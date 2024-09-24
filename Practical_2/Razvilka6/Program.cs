0fac120875d3d12b3e7dce9265b4dd05de55faea
Aidar <ajdar.mar@yandex.ru>
2024-09-24 22:31:32 +0300

    Made task №6

---
+++ Practical_2/Razvilka6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razvilka6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your age ");
+            var age = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine(age >= 18 ? "You can participate in the elections" : "You cannot participate in the elections");
+        }
+    }
+}
