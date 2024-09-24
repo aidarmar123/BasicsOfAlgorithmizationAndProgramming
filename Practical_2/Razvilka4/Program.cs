c5896b15557ac0d4555791a0f4ccdcb725c7f0fb
Aidar <ajdar.mar@yandex.ru>
2024-09-24 22:10:09 +0300

    Made task №3

---
+++ Practical_2/Razvilka4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razvilka4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input x: ");
+            var x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Answer is ");
+            if (x > 0)
+                Console.WriteLine(Math.Sin(x));
+            else
+                Console.WriteLine(Math.Cos(x));
+        }
+    }
+}
---
+++ Practical_2/Razvilka4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Razvilka4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Razvilka4")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("03d171b2-e7f8-4ce3-b893-5a2de01bb2f8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
