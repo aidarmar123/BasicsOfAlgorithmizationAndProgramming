abf0dd49521f0209fd98ad648b19fd38045db487
Марданов Айдар Булатович <202315@mck-ktits.ru>
2024-09-30 14:27:31 +0300

    Refresh Task 8

--- Practical_2/Razvilka8/Program.cs
+++ Practical_2/Razvilka8/Program.cs
@@ -8,12 +8,43 @@ namespace Razvilka8
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
             Console.Write("Input number day ");
             var number = Convert.ToUInt32(Console.ReadLine());
-            var date = DateTime.MinValue.AddDays(number-1);
+            var answer = "";
+
+            switch (number)
+            {
+                case 1:
+                    answer = "Monday";
+                    break;
+                case 2:
+                    answer = "Thuesday";
+                    break;
+                case 3:
+                    answer = "Wednesday";
+                    break;
+                case 4:
+                    answer = "Thusrday";
+                    break;
+                case 5:
+                    answer = "Friday";
+                    break;
+                case 6:
+                    answer = "Saturday";
+                    break;
+                case 7:
+                    answer = "Sunday";
+                    break;
+
+            }
+
+            Console.WriteLine(answer);
+            
+
+            var date = DateTime.MinValue.AddDays(number - 1);
             Console.WriteLine(date.ToString("dddd"));
         }
     }
