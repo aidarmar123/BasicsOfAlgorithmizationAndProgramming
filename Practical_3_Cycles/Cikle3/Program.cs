5cae22e6af0b6d7c831ad0740ac48ff78d98bbbe
Aidar <ajdar.mar@yandex.ru>
2024-10-01 13:16:14 +0300

    Finish Project

--- Practical_3_Cycles/Cikle3/Program.cs
+++ Practical_3_Cycles/Cikle3/Program.cs
@@ -10,6 +10,20 @@ namespace Cikle3
     {
         static void Main(string[] args)
         {
+            var a = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            if (a < 0)
+            {
+                for (int i = 3; i < 9; i++)
+                    sum += i - 2;
+            }
+            else
+            {
+                sum = 1;
+                for (int i = 2; i < 8; i++)
+                    sum *= i * i - a;
+            }
+            Console.WriteLine($"Sum is {sum}");
         }
     }
 }
