7a6832ea27bb7f9bc01fa790d5193ba8416570c6
Aidar <ajdar.mar@yandex.ru>
2024-10-01 12:45:36 +0300

    Made Task 1 and 2

---
+++ Practical_3_Cycles/Cikle1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cikle1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int answer=0;
+            for (int i = 1; i != sum/5; i++)
+            {
+                sum += i;
+                answer = i + 1;
+            }
+            Console.WriteLine($"Answer is {answer}");
+        }
+    }
+}
---
+++ Practical_3_Cycles/Cikle2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cikle2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" °C | °F ");
+            Console.WriteLine("__________");
+            for (int i = -50; i <=50; i++)
+            {
+                Console.WriteLine($"{i} | {(double)9/5*i +32}");
+            }
+        }
+    }
+}
