a2186ff947860e445bcb417005461dc39c7b3e21
Aidar <ajdar.mar@yandex.ru>
2024-10-01 13:01:51 +0300

    Made task 4

---
+++ Practical_3_Cycles/Cikl4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cikle4
+{
+    internal class Program
+    {
+        
+
+        private static int CountingTheAmount( int lastNum)
+        {
+            int sum= 0;
+            for (int i = 5; i<=lastNum; i += 5)
+            {
+                sum += i;
+            } 
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine(CountingTheAmount(100));
+                    break;
+                case 2:
+                    Console.WriteLine(CountingTheAmount(20));
+                    break;
+                case 3:
+                    Console.WriteLine(CountingTheAmount(10));
+                    break;
+                default:
+                    Console.WriteLine(100);
+                    break;
+                
+            }
+        }
+    }
+}
