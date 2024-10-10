4559d9954c6b3237f7886af41aad93099ce3a461
Aidar <ajdar.mar@yandex.ru>
2024-10-10 13:55:13 +0300

    Start array practical

--- Practical_3_Cycles/CiklPar1/Program.cs
+++ Practical_3_Cycles/CiklPar1/Program.cs
@@ -12,7 +12,9 @@ namespace CiklPar1
         {
             Console.Write("Input n: ");
             var n = Convert.ToInt32(Console.ReadLine());
-
+            int[] arru = new int[4];
+            int sumn = arru.Sum();
+            Console.WriteLine(sumn);
 
             double y = 0;
             for(int i = 2 * n + 1; i>=0; i -=2)
---
+++ Practical_4_Array/Odnom1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odnom1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrauNumber = new int[10];
+            var r = new Random();
+            for (int i = 0; i < arrauNumber.Length; i++)
+            {
+                arrauNumber[i] = r.Next(10);
+            }
+            int sum = 0;
+            for (int i = 0; i < arrauNumber.Length; i++)
+            {
+                if (i % 2 == 0)
+                    sum += arrauNumber[i]; 
+            }
+        }
+    }
+}
