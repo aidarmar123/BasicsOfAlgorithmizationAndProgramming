8e8778d3589c6168bfdd30ff5cb20ef88ae29320
Aidar <ajdar.mar@yandex.ru>
2024-10-16 13:35:49 +0300

    Made task 3

---
+++ Practical_4_Array/TwoDimensional/Dvym3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dvym3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4, 6];
+            Random random = new Random();
+
+            int[] arraySums = new int[matrix.GetLength(0)];
+            // Заполнение матрицы случайными значениями
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(1, 10);
+                    sum += matrix[i, j];
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+                arraySums[i] = sum;
+            }
+            Console.WriteLine("Array sum: "+string.Join(" ", arraySums));
+            Console.WriteLine("Maximum "+arraySums.Max());
+
+        }
+    }
+}
